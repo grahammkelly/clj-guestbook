0bf98fd65ef07066c0bc49d9b54f02c789958c2b
gkelly <gkelly@groupon.com>
2014-11-11 12:35:15 +0000

    Updated DB model. Added function to read entries from the DB, and to save a new entry

--- src/guestbook/models/db.clj
+++ src/guestbook/models/db.clj
@@ -1,12 +1,12 @@
 (ns guestbook.models.db
   (:require [clojure.java.jdbc :as sql])
   (:import java.sql.DriverManager)
+  ;; (:import java.util.Date)
 )
 
 (def db {:classname "org.sqlite.JDBC",
          :subprotocol "sqlite",
-         :subname "db.sq3"}
-)
+         :subname "db.sq3"})
 
 (defn create-guestbook-table []
   (sql/with-connection db
@@ -16,6 +16,18 @@
                       [:name "TEXT"]
                       [:message "TEXT"]
     )
-    (sql/do-commands "CREATE INDEX guestbook_timestamp_idx ON guestbook (timestamp)")
-  )
-)
+    (sql/do-commands "CREATE INDEX guestbook_timestamp_idx ON guestbook (timestamp)")))
+
+(defn read-guest-comments []
+  (sql/with-connection db
+    (sql/with-query-results res
+      ["SELECT * FROM guestbook ORDER BY timestamp DESC"]
+      (doall res)  ;;Returned results are lazy unless 'doall...'
+    )))
+
+(defn save-guest-comment [name message]
+  (sql/with-connection db
+    (sql/insert-values :guestbook
+                       [:name :message]
+                       [name message])))
+
