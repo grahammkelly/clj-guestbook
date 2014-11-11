0a361679b377ec715f01d86473e7dd75517c36b8
gkelly <gkelly@groupon.com>
2014-11-11 10:55:17 +0000

    Updated form - Changed how form is constructed (now Hiccup). Accepts input and renders errors.

--- src/guestbook/routes/home.clj
+++ src/guestbook/routes/home.clj
@@ -1,20 +1,58 @@
 (ns guestbook.routes.home
   (:require [compojure.core :refer :all]
             [guestbook.views.layout :as layout]
-            [hiccup.form :refer :all]))
+            [hiccup.form :refer :all])
+)
 
-(defn home []
+(defn home [& [name message error]]
   (layout/common
      [:h1 "Guestbook"]
      [:p "Welcome to the guestbook"]
+     [:p error]
+
+     (show-guests)
+
      [:hr]
-     [:form
-        [:p "Name: "]
-        [:input]
-        [:p "Message: "]
-        [:textarea {:rows 10 :cols 40}]
-     ]
-   ))
+     (form-to [:post "/"]
+              [:p "Name: "]
+              (text-field "name" name)
+              [:p "Message: "]
+              (text-area {:rows 10 :cols 40} "message" message)
+              [:br]
+              (submit-button "comment"))
+
+   )
+)
+
+(defn show-guests []
+  [:ul.guests
+    (for [{:keys [message name timestamp]}
+           [{:message "Howdy" :name "Bob" :timestamp nil}
+             {:message "Hello" :name "Tim" :timestamp nil}]]
+      [:li
+        [:blockquote message]
+        [:p "- " [:cite name]]
+        [:time timestamp]
+      ]
+    )
+  ]
+)
+
+(defn save-message [name message]
+  (cond
+     (empty? name)
+     (home name message "Oops, no name given!")
+     (empty? message)
+     (home name message "You need to give a message")
+     :else
+     (do
+       (println name message)
+       (home)
+     )
+  )
+)
 
 (defroutes home-routes
-  (GET "/" [] (home)))
+  (GET "/" [] (home))
+  (POST "/" [name message] (save-message name message))
+)
